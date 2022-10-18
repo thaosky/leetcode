5f66b2b6e2e65770bf2dfbcd68548e3a7fc375b6
lam <thaohsk@gmail.com>
2022-10-18 09:36:38 +0700

    BFS

--- src/stackQueue/queue/BFS.java
+++ src/stackQueue/queue/BFS.java
@@ -1,20 +1,18 @@
-package stackQueue.stack;
+package stackQueue.queue;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
-import java.util.Stack;
 
 /*
- * B1: Add đỉnh đầu vào stack và đánh dấu đã duyệt đỉnh đầu
- * B2: while(!stack.isEmpty()) {
-         *      u = stack.pop();
-         *      process(u) tùy thuộc từng bài toán
-         *      Add tất cả đỉnh v kề với u vào stack (v chưa được duyệt)
-         *      Đánh dấu đã duyệt v
- * }
+ *  B1: Add đỉnh đầu vào queue, đánh dấu đã thăm
+ *  B2: Khi trong queue còn element
+ *      - poll(u), xử lý j đó với u, tùy thuộc bài toán
+ *      - tìm các đỉnh v kề với u, và chưa được duyệt => cho v vào queue, đánh dấu đã duyệt
+ *
  */
-public class DFS {
-
+public class BFS {
     public static void main(String[] args) {
         int[][] graph = {
                 {0, 1, 0, 0, 0, 0, 0},
@@ -25,24 +23,20 @@ public class DFS {
                 {0, 0, 1, 1, 0, 0, 0},
                 {0, 0, 0, 1, 1, 0, 0}};
 
-        // B1: Add đỉnh đầu là đỉnh 0 vào stack, đánh dấu đã duyệt
-        Stack<Integer> stack = new Stack<>();
-        stack.add(0);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
         Set<Integer> visited = new HashSet<>();
         visited.add(0);
 
-        // Duyệt stack
-        while (!stack.isEmpty()) {
-            int u = stack.pop();
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
             System.out.println("Thăm đỉnh " + u);
-
             for (int v = 0; v < graph.length; v++) {
                 if (graph[u][v] == 1 && !visited.contains(v)) {
-                    stack.push(v);
+                    queue.add(v);
                     visited.add(v);
                 }
             }
-
         }
     }
 }
